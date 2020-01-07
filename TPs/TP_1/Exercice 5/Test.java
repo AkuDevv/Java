f073b255d40cb457b1a089446e60cb1983924681
mrUnarmed <safisaif755@gmail.com>
2020-01-07 20:04:11 +0100

    tps again

--- TPs/TP_1/Exercice 5/Test.java
+++ TPs/TP_1/Exercice 5/Test.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 class Test {
     public static void main(String[] args) {
         Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        FileStatistics fst = new FileStatistics(path + "/TP 1/Exercice 5/fichier1.txt");
+        FileStatistics fst = new FileStatistics(path + "/TP_1/Exercice 5/fichier1.txt");
         System.out.println("Le nombre de characteres: " + fst.getCharCount());
         System.out.println("Le nombre de mots: " + fst.getWordCount());
         System.out.println("Le nombre de phrases: " + fst.getSentanceCount());
